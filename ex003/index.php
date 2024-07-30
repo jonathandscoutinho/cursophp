6438250a8afe65f811dfbc9417b8a7d1253d47f1
Jonathan Coutinho <jonathandscoutinho@gmail.com>
2024-07-29 22:36:29 -0300

    update files

---
+++ ex003/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        //$nome = "Jonathan";
+        //echo "É verdade que seu nome é $nome?";
+
+        //$salario = 2500.75;
+        //print "Seu salario é de $salario?";
+
+        $nomeCompletoCliente = "Camel Case";
+        $telefone_contato_fornecedor = "Snake Case";
+    ?>
+</body>
+
+</html>
